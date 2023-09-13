6920531285c794218c70632d8782e93297f5bde4
batya <batyaserega@gmail.com>
2023-09-13 13:40:04 +0300

    Первый кормит

---
+++ APP-testUITests/APP_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  APP_testUITestsLaunchTests.swift
+//  APP-testUITests
+//
+//  Created by batya on 04.09.2023.
+//
+
+import XCTest
+
+final class APP_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
